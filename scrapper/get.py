20368b206476cc63141adaf768ff7432335a1851
Jijin K Haridas <jijinkh@protonmail.com>
2020-10-22 14:37:34 +0530

    added frontend + changes to flask app to support frontend

--- scrapper/get.py
+++ scrapper/get.py
@@ -16,5 +16,46 @@ def get():
     x["name"] = i[1]
     x["questscompleted"] = i[2]
     response.append(x)
+  conn.close()
+  return response
+def getSorted():  
+  import sqlite3
+
+  # Connect to database
+  conn = sqlite3.connect('../backend/database.db')
+
+  cur = conn.cursor()
+
+  cur.execute('SELECT * FROM PARTICIPANTS ORDER BY QUESTS DESC')
+  vals = cur.fetchall()
+
+  response = []
+  for i in vals:
+    x = {}
+    x["id"] = i[0]
+    x["name"] = i[1]
+    x["questscompleted"] = i[2]
+    response.append(x)
+
+  return response
+
+def getNameSorted():  
+  import sqlite3
+
+  # Connect to database
+  conn = sqlite3.connect('../backend/database.db')
+
+  cur = conn.cursor()
+
+  cur.execute('SELECT * FROM PARTICIPANTS ORDER BY NAME')
+  vals = cur.fetchall()
+
+  response = []
+  for i in vals:
+    x = {}
+    x["id"] = i[0]
+    x["name"] = i[1]
+    x["questscompleted"] = i[2]
+    response.append(x)
 
   return response
